c09c8f5a19d1876e438f50c9276815736382b720
andygarcia830 <andy@xurpas.com>
2023-02-21 16:37:35 +0800

    Added debug test

--- ksa_eis/services/test.py
+++ ksa_eis/services/test.py
@@ -6,7 +6,8 @@ import sys
 
 @frappe.whitelist()
 def test():
-    output=str(subprocess.check_output('source ../apps/ksa_eis/ksa_eis/services/test.sh',shell=True))
+   # output=str(subprocess.check_output('source ../apps/ksa_eis/ksa_eis/services/test.sh',shell=True))
+    output=str(subprocess.check_output('pwd',shell=True))
     print(f'PWD={output}')
 
 
