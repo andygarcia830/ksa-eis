24175ad12a79ae2870326385604319fc19deec21
andygarcia830 <andy@xurpas.com>
2023-02-22 13:42:09 +0800

    Added external XML integration prototype

--- ksa_eis/services/process_files.sh
+++ ksa_eis/services/process_files.sh
@@ -3,4 +3,4 @@ export PATH=$PATH:/usr/bin
 export FATOORA_HOME=../apps/ksa_eis/ksa_eis/zatca-einvoicing-sdk-232-R3.1.8/Apps
 export SDK_CONFIG=../apps/ksa_eis/ksa_eis/zatca-einvoicing-sdk-232-R3.1.8/Configuration/config.json
 #echo $1
-source $FATOORA_HOME/fatoora -validate -invoice "$1"
+$FATOORA_HOME/fatoora -validate -invoice "$1"
