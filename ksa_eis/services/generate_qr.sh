b6ab7cdfce2afcec45334c08f761b942f0ddbf53
Andy Garcia <andy@xurpas.com>
2023-02-28 18:35:14 +0800

    BugFixes

--- ksa_eis/services/generate_qr.sh
+++ ksa_eis/services/generate_qr.sh
@@ -10,7 +10,7 @@ export FATOORA_HOME=../apps/ksa_eis/ksa_eis/zatca-einvoicing-sdk-232-R3.1.8/Apps
 export SDK_CONFIG=../apps/ksa_eis/ksa_eis/zatca-einvoicing-sdk-232-R3.1.8/Configuration/config.json
 
 pathadd  $FATOORA_HOME
-pathadd ../apps/ksa_eis/ksa_eis/jdk-19.0.2/bin
+pathadd ../apps/ksa_eis/ksa_eis/jdk-11.0.2/bin
 
 echo $1
 $FATOORA_HOME/fatoora -qr -invoice "$1"
