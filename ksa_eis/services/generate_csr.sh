72285f6bbef6157b23d179764662d88901105898
Andy Garcia <andy@xurpas.com>
2023-02-28 16:48:05 +0800

    Added JRE Using Git LFS

--- ksa_eis/services/generate_csr.sh
+++ ksa_eis/services/generate_csr.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+
+pathadd() {
+    if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
+        export PATH="${PATH:+"$PATH:"}$1"
+    fi
+}
+
+
 export FATOORA_HOME=../apps/ksa_eis/ksa_eis/zatca-einvoicing-sdk-232-R3.1.8/Apps
 export SDK_CONFIG=../apps/ksa_eis/ksa_eis/zatca-einvoicing-sdk-232-R3.1.8/Configuration/config.json
 export INPUT_PATH=$1
+
+pathadd  $FATOORA_HOME
+pathadd ..apps/ksa_eis/ksa_eis/jre1.8.0_361/bin
 
 #command_string="fatoora -csr -csrConfig \"$INPUT_PATH/csr-config.properties\" -generatedCsr \"$INPUT_PATH/generated_csr.csr\""
 #echo $command_string
